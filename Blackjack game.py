bb54e770bb96d1dfe9493db4a67d760e94a937f3
Deepak Bisht <125358206+DeepakBisht788@users.noreply.github.com>
2024-12-02 18:09:35 +0530

    Blackjack game.py

--- Blackjack game.py
+++ Blackjack game.py
@@ -1,28 +1,21 @@
 import random
-
 def deal_card():
   """Returns a random card from the deck."""
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   card = random.choice(cards)
   return card
-
-
 def calculate_score(cards):
   """Take a list of cards and return the score calculated from the cards"""
-
   if sum(cards) == 21 and len(cards) == 2:
     return 0
   if 11 in cards and sum(cards) > 21:
     cards.remove(11)
     cards.append(1)
   return sum(cards)
-
-def compare(user_score, computer_score):
   
+def compare(user_score, computer_score):
   if user_score > 21 and computer_score > 21:
     return "You went over. You lose "
-
-
   if user_score == computer_score:
     return "Draw "
   elif computer_score == 0:
@@ -39,18 +32,12 @@ def compare(user_score, computer_score):
     return "You lose "
 
 def play_game():
-
-  #print(logo)
-
-  user_cards = []
+user_cards = []
   computer_cards = []
   is_game_over = False
-
   for _ in range(2):
     user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
-  
+    computer_cards.append(deal_card()) 
   while not is_game_over:
     user_score = calculate_score(user_cards)
     computer_score = calculate_score(computer_cards)
@@ -71,11 +58,8 @@ def play_game():
     computer_cards.append(deal_card())
     computer_score = calculate_score(computer_cards)
 
-  print(f"   Your final hand: {user_cards}, final score: {user_score}")
-  print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+  print(f"Your final hand: {user_cards}, final score: {user_score}")
+  print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
   print(compare(user_score, computer_score))
-
-
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
-  #clear()
   play_game()
